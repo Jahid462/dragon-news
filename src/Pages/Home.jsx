62ceeb1d7e36f6cffd1b435aad9b7017358f5f76
Jahid <jahid462540@gmail.com>
2023-11-15 00:06:36 +0600

    Navbar Added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import Navbar from "../Components/Navbar";
 
 const Home = () => {
     return (
         <div className="space-y-3">
             <Header></Header>
             <LatestNews></LatestNews>
+            <Navbar></Navbar>
         </div>
     );
 };
