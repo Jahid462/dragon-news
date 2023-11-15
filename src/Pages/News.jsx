f72bfadc2ff602af4d90ccbe8f1258110df4eb3b
Jahid <jahid462540@gmail.com>
2023-11-16 00:19:03 +0600

    News Details Added

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
 import RightSidebar from "../Components/RightSidebar";
@@ -6,15 +6,24 @@ import RightSidebar from "../Components/RightSidebar";
 const News = () => {
 
     const news = useParams()
+    const newses = useLoaderData()
+    const newsDetails = newses.find(aNews => aNews._id === news.id)
+    const {image_url, title, details} = newsDetails
+    console.log(newsDetails);
     
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <div className="grid grid-cols-4">
-                <div className="col-span-3">
+            <div className="grid grid-cols-4 gap-16">
+                <div className="col-span-3 space-y-3">
                     <h1 className="text-2xl font-semibold">Dragon News</h1>
-                    {news.id}
+                    <div className="border-2 rounded-md p-5 space-y-5">
+                        <img className="w-full mx-auto" src={image_url} alt="Image" />
+                        <h1 className="text-3xl font-bold">{title}</h1>
+                        <p>{details}</p>
+                        <Link to='/' className="btn btn-error text-white">All news in this category</Link>
+                    </div>
                 </div>
                 <div>
                     <RightSidebar></RightSidebar>
